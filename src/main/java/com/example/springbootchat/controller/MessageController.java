e6b68b5bbcf97d2f02d16a88ff7e19d28b7ee434
kazuki-shimura <sim202025@icloud.com>
2021-06-01 23:57:20 +0900

    create chat

---
+++ src/main/java/com/example/springbootchat/controller/MessageController.java
@@ -0,0 +1,26 @@
+package com.example.springbootchat.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.messaging.handler.annotation.DestinationVariable;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.springbootchat.model.MessageModel;
+import com.example.springbootchat.storage.UserStorage;
+
+@RestController
+public class MessageController {
+
+    @Autowired
+    private SimpMessagingTemplate simpMessagingTemplate;
+
+    @MessageMapping("/chat/{to}")
+    public void sendMessage(@DestinationVariable String to, MessageModel message) {
+        System.out.println("handling send message: " + message + " to: " + to);
+        boolean isExists = UserStorage.getInstance().getUsers().contains(to);
+        if (isExists) {
+            simpMessagingTemplate.convertAndSend("/topic/messages/" + to, message);
+        }
+    }
+}
